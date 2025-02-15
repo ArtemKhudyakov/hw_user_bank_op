7feb9c8a80746f904726749c37a25e6929d95d8e
Dr.T. <Aohudyakov@gmail.com>
2025-02-15 23:11:11 +0500

    добавил докстринг для функции test_get_mask_card_number()

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,6 +3,10 @@ import src.masks as masks
 
 
 def test_get_mask_card_number():
+    """Тестирование функции get_mask_card_number(). Функция должна принимать
+    номер карты в виде строки из 16 цифр в формате XXXX XX** **** XXXX,
+    где Х - это цифры. Если введены буквы или количество цифр меньше или больше 16
+    должна выводится ошибка ввода """
     assert masks.get_mask_card_number(
         '1111111111111111') == '1111 11** **** 1111'
     with pytest.raises(ValueError):
@@ -13,3 +17,6 @@ def test_get_mask_card_number():
     with pytest.raises(ValueError):
         assert masks.get_mask_card_number(
             'qqqqqqqqqqqqqqqq') == 'Неверный формат номера карты'
+
+
+def test_get_mask_account
