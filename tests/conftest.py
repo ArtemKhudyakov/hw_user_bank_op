88f205b1dd822eb04a501bc6e4f8e1f3cdb0df01
Dr.T. <Aohudyakov@gmail.com>
2025-03-08 19:50:16 +0500

    написал тест к test_transactions_data. линтеры ОК

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 
 
 @pytest.fixture
@@ -247,3 +248,24 @@ def transactions_by_rub() -> list[dict[str, object]]:
             "to": "Счет 14211924144426031657",
         },
     ]
+
+@pytest.fixture
+def first_transaction_json_data() -> dict[str, Any]:
+    transaction = {'id': 441945886, 'state': 'EXECUTED',
+                   'date': '2019-08-26T10:50:58.294041',
+                   'operationAmount': {'amount': '31957.58',
+                                       'currency': {'name': 'руб.',
+                                                    'code': 'RUB'}},
+                   'description': 'Перевод организации',
+                   'from': 'Maestro 1596837868705199',
+                   'to': 'Счет 64686473678894779589'}
+    return transaction
+
+
+
+    # current_file_path = Path(__file__).resolve()
+    # project_root_path = current_file_path.parent.parent
+    # file_path = f"{project_root_path}/data/operations.json"
+    # with open(file_path, "r", encoding="utf-8") as f:
+    #     return json.load(f)
+
---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+import src.utils as utils
+from typing import Any
+
+
+from tests.conftest import first_transaction_json_data
+
+
+def test_transactions_data(first_transaction_json_data:dict[str, Any]) -> None:
+    transactions_list = utils.transactions_data("data/operations.json")
+    if transactions_list:
+        assert transactions_list[0] == first_transaction_json_data
+
+def test_transactions_data_empty()-> None:
+    transactions_list = utils.transactions_data("empty.json")
+    assert transactions_list == []
