967555672bc1111a89cbbf3c54e04106a8ed1e28
Dr.T. <Aohudyakov@gmail.com>
2025-03-08 23:00:29 +0500

    добавил док-стринги к тестам

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -9,12 +9,14 @@ import src.external_api as external_api
 def test_convert_into_rub_1(
     first_transaction_json_data: dict[str, Any],
 ) -> None:
+    """Тест функции convert_into_rub, если транзакция в рублях"""
     assert external_api.convert_into_rub(first_transaction_json_data) == str(
         31957.58
     )
 
 
 def test_convert_into_rub_2() -> None:
+    """Тест функции convert_into_rub, если транзакция пустая"""
     with pytest.raises(ValueError):
         external_api.convert_into_rub({})
 
@@ -23,6 +25,7 @@ def test_convert_into_rub_2() -> None:
 def test_convert_into_rub_3(
     mock_request: Mock, two_transactions_json_data: list[dict[str, Any]]
 ) -> None:
+    """Тест функции convert_into_rub, если внешний ресурс отвечает с ошибкой"""
     transaction = two_transactions_json_data[1]
     mock_response = Mock()
     mock_response.status_code = 500
@@ -36,6 +39,7 @@ def test_convert_into_rub_3(
 def test_convert_into_rub_4(
     mock_request: Mock, two_transactions_json_data: list[dict[str, Any]]
 ) -> None:
+    """Тест функции convert_into_rub, если транзакция в долларах"""
     transaction = two_transactions_json_data[1]
     mock_response = Mock()
     mock_response.status_code = 200
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,11 +6,15 @@ import src.utils as utils
 def test_transactions_data(
     first_transaction_json_data: dict[str, Any],
 ) -> None:
+    """Тест функции test_transactions_data, если в функцию передаются верные
+значения"""
     transactions_list = utils.transactions_data("data/operations.json")
     if transactions_list:
         assert transactions_list[0] == first_transaction_json_data
 
 
 def test_transactions_data_empty() -> None:
+    """Тест функции test_transactions_data, если в функцию передается неверный
+путь к файлу"""
     transactions_list = utils.transactions_data("empty.json")
     assert transactions_list == []
