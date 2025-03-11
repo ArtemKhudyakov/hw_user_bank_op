c4a3d3e4aa6228d1744f2f493a82e6d83a8de40d
Dr.T. <Aohudyakov@gmail.com>
2025-03-12 00:42:27 +0500

    выполнено логирование модулей masks.py и utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,33 @@
+import logging
+from pathlib import Path
+
 import src.decorators as decorators
+
+current_file_path = Path(__file__).resolve()
+project_root_path = current_file_path.parent.parent
+
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(
+    f"{project_root_path}/logs/masks.log", encoding="utf-8", mode="w"
+)
+file_formater = logging.Formatter(
+    "%(asctime)s - %(name)s -%(funcName)s - %(lineno)d - %(levelname)s -"
+    " %(message)s"
+)
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
 
 
 @decorators.log("logs/my_log.txt")
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает номер банковской карты в виде строки без пробелов,
     возвращает маскированный номер карты в формате XXXX XX** **** XXXX"""
+    logger.debug("Проверяем правильность ввода номера карты")
     if card_number.isdigit() is True and len(card_number) == 16:
         card_number_as_list = [i for i in card_number]
+        logger.debug("Маскируем номер карты")
         for i in range(6, 12):
             card_number_as_list[i] = "*"
         masked_card_number = (
@@ -19,8 +40,12 @@ def get_mask_card_number(card_number: str) -> str:
             + "".join(card_number_as_list[12:16])
         )
     else:
+        logger.error("Введен неверный формат номера карты")
         raise ValueError("Неверный формат номера карты")
 
+    logger.debug(
+        f"Возвращаем замаскированный номер карты {masked_card_number}"
+    )
     return masked_card_number
 
 
@@ -31,10 +56,16 @@ def get_mask_account(acc_number: str) -> str:
     где X — это цифра номера. То есть видны только последние 4 цифры номера,
     а перед ними — две звездочки
     """
+    logger.debug("Проверяем правильность ввода номера счета")
     if acc_number.isdigit() is True and len(acc_number) == 20:
+        logger.debug("Маскируем номер счета")
         masked_acc_number = (
             "**" + acc_number[(len(acc_number) - 4) : (len(acc_number) + 1)]
         )
+        logger.debug(
+            f"Возвращаем замаскированный номер счета {masked_acc_number}"
+        )
         return masked_acc_number
     else:
+        logger.error("Введен неверный формат номера счета")
         raise ValueError("Неверный формат номера счета")
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,22 @@
 import json
+import logging
 from pathlib import Path
 from typing import Any, List, Optional
+
+current_file_path = Path(__file__).resolve()
+project_root_path = current_file_path.parent.parent
+
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(
+    f"{project_root_path}/logs/utils.log", encoding="utf-8", mode="w"
+)
+file_formater = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
 
 
 def transactions_data(
@@ -14,9 +30,12 @@ def transactions_data(
     project_root_path = current_file_path.parent.parent
     file_path = f"{project_root_path}/{path_to_json_file}"
     try:
+        logger.debug(f'Загружаем данные из файла "{path_to_json_file}"')
         with open(file_path, "r", encoding="utf-8") as f:
             json_data = json.load(f)
-    except Exception:
+    except Exception as ex:
+        logger.error(f"Произошла ошибка: {ex}")
         json_data = []
     finally:
+        logger.debug(f"Возвращаем результат --> {json_data}")
         return json_data
