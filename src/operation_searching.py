5b29fd3ad430a234ba6079e9d9305d568060ebb3
Dr.T. <Aohudyakov@gmail.com>
2025-03-21 10:36:29 +0500

    реализовал функцию op_counter_by_category

--- src/operation_searching.py
+++ src/operation_searching.py
@@ -1,12 +1,10 @@
 import re
-from collections import Counter, defaultdict
+from collections import Counter
 
-from pyflakes.checker import counter
-from unicodedata import category
 
 from src.data_reader import xlsx_reader
 
-operations_list = xlsx_reader('data/transactions_excel.xlsx')
+operations_list = xlsx_reader("data/transactions_excel.xlsx")
 #     [
 #     {
 #         "id": 650703.0,
@@ -45,34 +43,43 @@ operations_list = xlsx_reader('data/transactions_excel.xlsx')
 
 
 def op_searching_by_description(
-        string_for_searching: str, operations_list: list[dict[str, str]]
+    string_for_searching: str, operations_list: list[dict[str, str]]
 ) -> list[dict[str, str]]:
-    """Принимает список словарей с данными о банковских операциях и строку поиска, а возвращать список словарей,
-у которых в описании есть данная строка."""
+    """Принимает список словарей с данными о банковских операциях и строку
+    поиска, а возвращать список словарей, у которых в описании есть данная
+    строка."""
     operations_by_description = [
         operation
         for operation in operations_list
-        if re.search(string_for_searching, operation["description"], re.IGNORECASE)
+        if re.search(
+            string_for_searching, operation["description"], re.IGNORECASE
+        )
     ]
     return operations_by_description
 
-srch_categories_list = ['Открытие вклада', 'Перевод с карты на карту']
-# принимать список словарей с данными о банковских операциях и список категорий операций, а возвращать словарь, в
-# котором ключи — это названия категорий, а значения — это количество операций в каждой категории.
-def op_counter_by_category(operations_list: list[dict[str, str]], srch_categories_list=None):
+
+srch_categories_list = ["ОткрытиЕ Вклада", "ПЕРЕВОД с карты на карту", "апапа"]
+"""Функция принимает список словарей с данными о банковских операциях и
+список категорий операций, возвращать словарь, в котором ключи — это названия
+категорий, а значения — это количество операций в каждой категории."""
+
+
+def op_counter_by_category(
+    operations_list: list[dict[str, str]], srch_categories_list
+):
     srch_counter = dict()
-    op_category_list = [operation["description"] for operation in operations_list]
+    op_category_list = [
+        operation["description"] for operation in operations_list
+    ]
     category_counter = Counter(op_category_list)
 
     for i in srch_categories_list:
-        if i in op_category_list:
-            for k, v in category_counter.items():
-                if k in srch_categories_list:
-                    srch_counter[k] = v
+        if i.capitalize() in list(category_counter.keys()):
+            srch_counter[i.capitalize()] = category_counter[i.capitalize()]
         else:
-            srch_counter[i] = "операция не найдена"
-    print(category_counter)
-    print(srch_counter)
+            srch_counter[i] = "Операция не найдена"
+
+    return srch_counter
 
 
-op_counter_by_category(operations_list, srch_categories_list)
+print(op_counter_by_category(operations_list, srch_categories_list))
